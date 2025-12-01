cd3e614f7c94734e54ee02e84eefeb2b1a7a7c4e
François Roland <francois.roland@heh.be>
2025-12-01 18:14:39 +0100

    Create backend to serve course list.

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import caching from '@fastify/caching'
+import { COURSES } from './courses.js'
+
+const fastify = Fastify({
+  logger: true
+})
+
+await fastify.register(cors, {
+  origin: [
+    'http://localhost:4173',
+    'http://localhost:5173',
+  ]
+})
+
+await fastify.register(caching, {
+  privacy: caching.privacy.NOCACHE,
+})
+
+fastify.get('/api/courses', async (request, reply) => {
+  reply.etag("constant-course-list")
+  return COURSES
+})
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
